75cb0005c9153c7134d8706b6e287121d66686bf
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-15 10:42:46 +0200

    Update nav.part.php

--- views/partials/nav.part.php
+++ views/partials/nav.part.php
@@ -14,10 +14,21 @@
        </div>
        <div class="collapse navbar-collapse navbar-right" id="menu">
           <ul class="nav navbar-nav">
-            <li class=" active lien"><a href="index.php"><i class="fa fa-home sr-icons"></i> Home</a></li>
-            <li class=" lien"><a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
-            <li class=" lien"><a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
-            <li><a href="contact.php"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
+            <li class=" <?=(esOpcionMenuActiva("index")? "active": "");?> lien">
+                <a href="<?=(esOpcionMenuActiva("index")? "#" : "/");?>">
+                    <i class="fa fa-home sr-icons"></i> Home
+                </a></li>
+            <li class="<?=(esOpcionMenuActiva("about")?"active": "");?> lien">
+                <a href="<?=(esOpcionMenuActiva("about")?"#" : "/about.php");?>">
+                    <i class="fa fa-bookmark sr-icons"></i> About
+                </a></li>
+            <li class="<?=(esOpcionMenuActiva("blog")?"active": "");?> lien">
+                <a href="<?=(esOpcionMenuActiva("blog")?"#" : "/blog.php");?>">
+                    <i class="fa fa-file-text sr-icons"></i> Blog</a></li>
+            <li class="<?=(esOpcionMenuActiva("contact")?"active": "");?> lien"> 
+            <a href="<?=(esOpcionMenuActiva("contact")?"#" : "/contact.php");?>">
+            <i class="fa fa-bookmark sr-icons">
+                </i> Contact</a></li>
           </ul>
        </div>
      </div>
