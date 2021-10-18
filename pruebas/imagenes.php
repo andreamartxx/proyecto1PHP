c557f57c3f76d9307c7bd55cf3bc05994ccd1b34
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-18 09:47:38 +0200

    Create imagenes.php

---
+++ pruebas/imagenes.php
@@ -0,0 +1,19 @@
+<?php
+
+    $idCategoria = 1;
+
+    $imagenes = [
+            ['nombre' => '1.jpg', 'alt' => 'Foto 1'],
+
+            ['nombre' => '2.jpg', 'alt' => 'Foto 2'],
+
+            ['nombre' => '3.jpg', 'alt' => 'Foto 3'],
+
+            ['nombre' => '4.jpg', 'alt' => 'Foto 4'],
+
+            ['nombre' => '5.jpg', 'alt' => 'Foto 5']
+    ];
+
+    include __DIR__ . "/imagenes.view.php";
+
+?>
