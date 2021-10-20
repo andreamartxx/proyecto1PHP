4d3af61784a5dfdbe5f3db63224c1cfb3baac391
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-20 10:38:14 +0200

    Update utils.php

--- utils/utils.php
+++ utils/utils.php
@@ -36,6 +36,14 @@
           return false;
      }
 
+     function sanitizeInput(string $data): string {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+
+          return $data;
+     }
+
 
 ?>
 
