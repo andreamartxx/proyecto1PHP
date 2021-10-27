2dc945ea79544c84c3b05d5bfcf2ec0f8452280c
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-27 08:32:18 +0200

    updatev5.1

--- galeria.php
+++ galeria.php
@@ -1,9 +1,10 @@
 <?php
     $title = "Galería";
     require_once "./utils/utils.php";
-    require_once "./entity/ImagenGaleria.php";
+    require_once "./entity/imagenGaleria.php";
     require_once "./utils/File.php";
     require_once "./exceptions/FileException.php";
+    require_once "./utils/SimpleImage.php";
 
     $info = $description = $urlImagen = "";
     $descripcionError = $imagenErr = $hayErrores = false;
@@ -15,9 +16,28 @@
             if(empty($_POST)){
                throw new FileException('Se ha producido un error al procesar el formulario');
             }
-            $imageFile = new File("imagen", array("image/jpeg", "image/jpg", "image/png"), (2 **1024));
+            $imageFile = new File("imagen", array("image/jpeg", "image/jpg", "image/png"), (2 *1024 *1024));
 
             $imageFile->saveUploadedFile(ImagenGaleria::RUTA_IMAGENES_GALLERY);
+
+            try{
+
+                $simpleImagen = new \claviska\SimpleImage();
+         
+             
+             $simpleImagen
+                 ->fromFile(ImagenGaleria::RUTA_IMAGENES_GALLERY. $imageFile->getFileName()) 
+                 ->resize(975, 525)                          
+                 ->toFile(ImagenGaleria::RUTA_IMAGENES_PORTFOLIO. $imageFile->getFileName())                        
+                 ->resize(650, 350)                          
+                 ->toFile(ImagenGaleria::RUTA_IMAGENES_GALLERY. $imageFile->getFileName())                        
+                 ->resize(445, 240)                          
+                 ->toFile(ImagenGaleria::RUTA_IMAGENES_GALLERY. $imageFile->getFileName());                        
+             
+             }catch(Exception $err){
+                 $errores[] = $err->getMessage();
+                 $imagenErr = true;
+             }
 
         }catch(FileException $fe){
             $errores[] = $fe -> getMessage();
