2005560d76a98c917ca2cc8a12cd64060303b7ed
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-29 08:50:38 +0200

    updatev5.2

---
+++ entity/Asociado.php
@@ -0,0 +1,107 @@
+<?php
+
+    class Asociado{
+
+        const RUTA_IMAGENES_LOGO = 'images/index/';
+
+        /**
+         * Undocumented variable
+         *
+         * @var string
+         */
+
+        private $nombre;
+
+        /**
+         * Undocumented variable
+         *
+         * @var string
+         */
+        private $logo;
+
+        /**
+         * Undocumented variable
+         *
+         * @var string
+         */
+        private $descripcion;
+        
+
+        public function __construct(string $nombre, string $logo, string $descripcion = ""){
+            $this->nombre = $nombre;
+            $this->logo = $logo;
+            $this->descripcion = $descripcion;           
+
+        }
+    
+    
+        /**
+         * Get the value of nombre
+         */ 
+        public function getNombre()
+        {
+                return $this->nombre;
+        }
+
+        /**
+         * Set the value of nombre
+         *
+         * @return  self
+         */ 
+        public function setNombre($nombre)
+        {
+                $this->nombre = $nombre;
+
+                return $this;
+        }
+
+        /**
+         * Get de value of logo
+         *
+         * @return string
+         */
+        public function getLogo()
+        {
+
+                return $this->logo;
+        }
+        
+        /**
+         * Set the value of logo
+         *
+         * @return self
+         */
+        public function setLogo($logo)
+        {
+                $this->logo = $logo;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of descripcion
+         *
+         * @return  string
+         */ 
+        public function getDescripcion()
+        {
+                return $this->descripcion;
+        }
+
+        /**
+         * Set the value of descripcion
+         *
+         * @param  string  $descripcion  Undocumented variable
+         *
+         * @return  self
+         */ 
+        public function setDescripcion(string $descripcion)
+        {
+                $this->descripcion = $descripcion;
+
+                return $this;
+        }
+
+    }
+
+?>
