515e6956174ad334c088cc57568270b36f1176fd
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-11-10 08:51:38 +0100

    v6

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,85 +1,18 @@
-
 <?php
-
   include __DIR__ . "/partials/inicio-doc.part.php";
   include __DIR__ . "/partials/nav.part.php";
-
-?>  
-
+?>
 <!-- Principal Content Start -->
    <div id="contact">
    	  <div class="container">
    	    <div class="col-xs-12 col-sm-8 col-sm-push-2">
-       	   <h1>CONTACT US</h1>
+		<h1>CONTACT US</h1>
        	   <hr>
-       	   <p>Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-	       	
-			<?php
-  				include __DIR__ . "/partials/show-messages.part.php";
-  			?>
-
-			  <form class="form-horizontal" action="/contact.php" method="POST">
-
-				<div class="form-group">
-
-				<div class="col-xs-6">
-
-					<label for="firstName" class="label-control">First Name</label>
-
-					<input class="form-control <?=($firstNameError ? "has-error" : "" );?>" type="text" name="firstName" id="firstName" value="<?=$firstName?>">
-
-				</div>
-
-					<div class="col-xs-6">
-
-						<label for="lastName" class="label-control">Last Name</label>
-
-						<input class="form-control" type="text" name="lastName" id="lastName" value="<?=$lastName?>">
-
-					</div>
-
-				</div>
-
-					<div class="form-group">
-
-						<div class="col-xs-12">
-
-							<label for="email" class="label-control">Email</label>
-
-							<input class="form-control <?=($emailErr ? "has-error" : "" );?>" type="text" name="email" id="email" value="<?=$email?>">
-
-						</div>
-
-					</div>
-
-					<div class="form-group">
-
-						<div class="col-xs-12">
-
-							<label for="subject" class="label-control">Subject</label>
-
-							<input class="form-control <?=($subjectError ? "has-error" : "" );?>" type="text" name="subject" id="subject" value="<?=$subject?>">
-
-						</div>
-
-					</div>
-
-					<div class="form-group">
-
-						<div class="col-xs-12">
-
-							<label for="message" class="label-control">Message</label>
-
-							<textarea class="form-control" name="message" id="message" value="<?=$message?>"></textarea>
-
-							<button class="pull-right btn btn-lg sr-button">SEND</button>
-
-						</div>
-
-					</div>
-
-				</form>
-			
+			  <p>Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+					<?php
+                include __DIR__ . "/partials/show-messages.part.php";
+        	?>
+           <?=$form->render();?>
 	       <hr class="divider">
 	       <div class="address">
 	           <h3>GET IN TOUCH</h3>
@@ -106,15 +39,6 @@
    </div>
 <!-- Principal Content Start -->
 
-<!-- Jquery -->
-   <script type="text/javascript" src="js/jquery.min.js"></script>
-   <!-- Bootstrap core Javascript -->
-   <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-   <!-- Plugins -->
-   <script type="text/javascript" src="js/jquery.easing.min.js"></script>
-   <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
-   <script type="text/javascript" src="js/scrollreveal.min.js"></script>
-   <script type="text/javascript" src="js/script.js"></script>
-</body>
-</html>
-
+<?php
+  include __DIR__ . "/partials/fin-doc.part.php";
+?>
