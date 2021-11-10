a5ea600d1aa6d7db3b10ec68056e967a53d735f7
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-11-10 08:48:14 +0100

    v6

--- entity/Asociado.php
+++ entity/Asociado.php
@@ -1,116 +1,113 @@
-<?php
+<?php 
 
-    class Asociado{
+class Asociado
+{
+    const RUTA_IMAGENES_ASOCIADO = 'images/index/';
+    /**
+     * @var string
+     */
+    private $nombre;
 
-        const RUTA_IMAGENES_LOGO = 'images/index/';
+    /**
+     * @var string
+     */
+    private $logo;
 
-        /**
-         * Undocumented variable
-         *
-         * @var string
-         */
+    /**
+     * @var string
+     */
+    private $descripcion;
 
-        private $nombre;
-
-        /**
-         * Undocumented variable
-         *
-         * @var string
-         */
-        private $logo;
-
-        /**
-         * Undocumented variable
-         *
-         * @var string
-         */
-        private $description;
-        
-
-        public function __construct(string $nombre, string $logo, string $description = ""){
-            $this->nombre = $nombre;
-            $this->logo = $logo;
-            $this->description = $description;           
-
-        }
-
-        public function getUrlAsociados() : string
-
-        {
-
-            return self::RUTA_IMAGENES_LOGO . $this->getLogo();
-
-        }
-    
-        /**
-         * Get the value of nombre
-         */ 
-        public function getNombre()
-        {
-                return $this->nombre;
-        }
-
-        /**
-         * Set the value of nombre
-         *
-         * @return  self
-         */ 
-        public function setNombre($nombre)
-        {
-                $this->nombre = $nombre;
-
-                return $this;
-        }
-
-        /**
-         * Get de value of logo
-         *
-         * @return string
-         */
-        public function getLogo()
-        {
-
-                return $this->logo;
-        }
-        
-        /**
-         * Set the value of logo
-         *
-         * @return self
-         */
-        public function setLogo($logo)
-        {
-                $this->logo = $logo;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of descripcion
-         *
-         * @return  string
-         */ 
-        public function getDescription()
-        {
-                return $this->description;
-        }
-
-        /**
-         * Set the value of descripcion
-         *
-         * @param  string  $descripcion  Undocumented variable
-         *
-         * @return  self
-         */ 
-        public function setDescription(string $description)
-        {
-                $this->description = $description;
-
-                return $this;
-        }
-
-        
-
+    /**
+     * @param string $nombre
+     * @param string $logo
+     * @param string $descripcion
+     */
+    public function __construct(string $nombre, string $logo, string $descripcion = ""){
+        $this->nombre = $nombre;
+        $this->logo = $logo;
+        $this->descripcion = $descripcion;
     }
 
-?>
+    /**
+     * Get the value of nombre
+     *
+     * @return  string
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param  string  $nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre(string $nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of logo
+     *
+     * @return  string
+     */ 
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    /**
+     * Set the value of logo
+     *
+     * @param  string  $logo
+     *
+     * @return  self
+     */ 
+    public function setLogo(string $logo)
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descripcion
+     *
+     * @return  string
+     */ 
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set the value of descripcion
+     *
+     * @param  string  $descripcion
+     *
+     * @return  self
+     */ 
+    public function setDescripcion(string $descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Devuelve el path a las imágenes del asociado
+     *
+     * @return string
+     */
+    public function getUrlImagen() : string
+    {
+        return self::RUTA_IMAGENES_ASOCIADO . $this->getLogo();
+    }
+}
