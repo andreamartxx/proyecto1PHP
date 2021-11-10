1c2f11e9cfcc33d5d9cc3b2a1fa1cb261b0c5d12
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-11-10 08:47:10 +0100

    v6

--- galeria.php
+++ galeria.php
@@ -1,64 +1,73 @@
 <?php
     $title = "Galería";
     require_once "./utils/utils.php";
-    require_once "./entity/imagenGaleria.php";
-    require_once "./utils/File.php";
+    require_once "./utils/Forms/TextareaElement.php";
+    require_once "./utils/Forms/ButtonElement.php";
+    require_once "./utils/Forms/FileElement.php";
+    require_once "./utils/Forms/FormElement.php";
+    require_once "./utils/Forms/custom/MyFormGroup.php";
+    require_once "./utils/Forms/custom/MyFormControl.php";
+    require_once "./utils/Validator/NotEmptyValidator.php";
+    require_once "./utils/Validator/MimetypeValidator.php";
+    require_once "./utils/Validator/MaxSizeValidator.php";    
     require_once "./exceptions/FileException.php";
     require_once "./utils/SimpleImage.php";
+    require_once "./entity/ImagenGaleria.php";
+    
+    $info = $urlImagen = "";
 
-    $info = $description = $urlImagen = "";
-    $descripcionError = $imagenErr = $hayErrores = false;
-    $errores = [];
+    $description = new TextareaElement();
+    $description
+     ->setName('descripcion')
+     ->setId('descripcion')
+     ->setValidator(new NotEmptyValidator('La descripción es obligatoria', true));
+    $descriptionWrapper = new MyFormControl($description, 'Descripción', 'col-xs-12');
 
-    if("POST" === $_SERVER["REQUEST_METHOD"]){
-        
-        try{
-            if(empty($_POST)){
-               throw new FileException('Se ha producido un error al procesar el formulario');
-            }
-            $imageFile = new File("imagen", array("image/jpeg", "image/jpg", "image/png"), (2 *1024 *1024));
+    $fv = new MimetypeValidator(['image/jpeg', 'image/jpg', 'image/png'], 'Formato no soportado', true);
+    
+    $fv->setNextValidator(new MaxSizeValidator(2 * 1024 * 1024, 'El archivo no debe exceder 2M', true));
+    $file = new FileElement();
+    $file
+      ->setName('imagen')
+      ->setId('imagen')
+      ->setValidator($fv);
 
-            $imageFile->saveUploadedFile(ImagenGaleria::RUTA_IMAGENES_GALLERY);
+    $labelFile = new LabelElement('Imagen', $file);
 
-            try{
+    $b = new ButtonElement('Send');
+    $b->setCssClass('pull-right btn btn-lg sr-button');
 
-                $simpleImagen = new \claviska\SimpleImage();
-         
-             
-             $simpleImagen
-                 ->fromFile(ImagenGaleria::RUTA_IMAGENES_GALLERY. $imageFile->getFileName()) 
-                 ->resize(975, 525)                          
-                 ->toFile(ImagenGaleria::RUTA_IMAGENES_PORTFOLIO. $imageFile->getFileName())                        
-                 ->resize(650, 350)                          
-                 ->toFile(ImagenGaleria::RUTA_IMAGENES_GALLERY. $imageFile->getFileName())                        
-                 ->resize(445, 240)                          
-                 ->toFile(ImagenGaleria::RUTA_IMAGENES_GALLERY. $imageFile->getFileName());                        
-             
-             }catch(Exception $err){
-                 $errores[] = $err->getMessage();
-                 $imagenErr = true;
-             }
+    $form = new FormElement('', 'multipart/form-data');
+    $form
+    ->setCssClass('form-horizontal')
+    ->appendChild($labelFile)
+    ->appendChild($file)
+    ->appendChild($descriptionWrapper)
+    ->appendChild($b);
 
-        }catch(FileException $fe){
-            $errores[] = $fe -> getMessage();
-            $imagenErr = true;
-        }
-       
-        $description = sanitizeInput(($_POST["description"] ?? ""));
-
-        if(empty($description)){
-            $errores[] = "La descripción es obligatoria";
-            $descripcionError = true;
-        }
-        
-        if(0 == count($errores)){
-            $info = 'Imagen enviada correctamente: ';
-            $urlImagen = ImagenGaleria::RUTA_IMAGENES_GALLERY . $imageFile->getFileName();
-            $description = "";
+    if ("POST" === $_SERVER["REQUEST_METHOD"]) {
+        $form->validate();
+        if (!$form->hasError()) {
+          try {
+            $file->saveUploadedFile(ImagenGaleria::RUTA_IMAGENES_GALLERY);  
+              // Create a new SimpleImage object
+              $simpleImage = new \claviska\SimpleImage();
+              $simpleImage
+              ->fromFile(ImagenGaleria::RUTA_IMAGENES_GALLERY . $file->getFileName())  
+              ->resize(975, 525)
+              ->toFile(ImagenGaleria::RUTA_IMAGENES_PORTFOLIO . $file->getFileName())
+              ->resize(650, 350)
+              ->toFile(ImagenGaleria::RUTA_IMAGENES_GALLERY . $file->getFileName()); 
+              $info = 'Imagen enviada correctamente'; 
+              $urlImagen = ImagenGaleria::RUTA_IMAGENES_GALLERY . $file->getFileName();
+              $form->reset();
+            
+          }catch(Exception $err) {
+              $form->addError($err->getMessage());
+              $imagenErr = true;
+          }
         }else{
-            $info =  "Datos erróneos";
+          
         }
-
     }
-
     include("./views/galeria.view.php");
