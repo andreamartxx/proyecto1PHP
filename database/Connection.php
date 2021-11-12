4461f6c8ace78441ec7e18b3420d177c0f57f067
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-11-12 10:23:22 +0100

    v7_12nov

--- database/Connection.php
+++ database/Connection.php
@@ -1,26 +1,18 @@
 <?php
 
     class Connection{
-        public static function make(){
+        public static function make($config){
             try{
 
-                $opciones = [
-                    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_PERSISTENT => true
-                ];
-
                 $connection = new PDO(
-                    'mysql:host=localhost;dbname=proyecto1;charset=utf8',
-                    'proyecto1',
-                    'sa',
-                    $opciones
-                );
+                    $config['connection'].';dbname' . $config['name'],
+                    $config['username'],
+                    $config['password'],
+                    $config['options']);
+                }catch(PDOException $PDOException){
+                    die($PDOException->getMessage());
 
-            }catch(PDOException $PDOException){
-                die($PDOException->getMessage());
-            }
-
+                }
             return $connection;
         }
     }
