ece3bdc22404415bb0df3bc28b3fbcd1d0c715e8
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-11-12 10:31:45 +0100

    v7_12nov

---
+++ core/App.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once "./database/Connection.php";
+    require_once "./database/QueryBuilder.php";
+
+    class App{
+        private static $container = [];
+
+        public static function bind(string $key, $value){
+            static::$container[$key] = $value;
+        }
+
+        public static function get(string $key){
+            if(!array_key_exists($key, static::$container)){
+                throw new AppException("No se ha encontrado la clave $key en el contenedor");
+            }
+            return static::$container[$key];
+        }
+    }
