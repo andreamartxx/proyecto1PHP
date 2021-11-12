4aebbfc887aefdd54661eb84868512f3e1d30aa0
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-11-12 10:22:44 +0100

    v7_12nov

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -4,18 +4,22 @@
     require_once "./database/Connection.php";
 
     class QueryBuilder{
+
+        private $connection;
+
         public function __construct($connection)
         {
-            $this->connection = $connection::make();
+            $this->connection = $connection;
         }
 
         public function findAll(string $table, string $classEntity){
-            $sql = "SELECT * FROM $this->table";
+            $sql = "SELECT * FROM $table";
+
             try{
                 $pdoStatement = $this->connection->prepare($sql);
                 $pdoStatement->execute();
                 $pdoStatement->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE,
-                    "ProyectoWeb\\entity\\" . $this->classEntity);
+                    $classEntity);
                 return $pdoStatement->fetchAll();
             }catch(\PDOException $pdoException){
 
