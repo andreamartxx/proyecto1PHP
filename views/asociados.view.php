1e338185345c6fc44ac2973dbdccb75a73f5b8ba
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-11-22 09:43:49 +0100

    v7_22nov

--- views/asociados.view.php
+++ views/asociados.view.php
@@ -1,60 +1,24 @@
-
 <?php
-
-include __DIR__ . "/partials/inicio-doc.part.php";
-include __DIR__ . "/partials/nav.part.php";
-
-?>  
-
-<!-- Principal Content Start -->
- <div id="asociados">
-       <div class="container">
-         <div class="col-xs-12 col-sm-8 col-sm-push-2">
+  include __DIR__ . "/partials/inicio-doc.part.php";
+  include __DIR__ . "/partials/nav.part.php";
+  ?>
+<div id="asociados">
+    <div class="container">
+        <div class="col-xs-12 col-sm-8 col-sm-push-2">
             <h1>ASOCIADOS</h1>
             <hr>
-
             <?php
-  				      include __DIR__ . "/partials/show-messages.part.php";
-  			?>
-
-            <?php if(("POST" === $_SERVER["REQUEST_METHOD"])&&(empty($errores))) : ?>
+                include __DIR__ . "/partials/show-messages.part.php";
+            ?>
+            <?php if (("POST" === $_SERVER["REQUEST_METHOD"]) && (!$form->hasError())) : ?>
                 <a href='<?=$urlImagen?>' target='_blank'>Ver Imagen</a>
-            <?php endif;?>
-
-            <form class="form-horizontal" action="/asociados.php" method="POST" enctype="multipart/form-data">
-
-              <div class="form-group">
-
-                <div class="col-xs-12">
-
-                    <label for="imagen" class="label-control">Imagen</label>
-
-                    <input class="form-control-file" type="file" name="imagen">
-
-                </div>
-              </div>
-
-                <div class="form-group">
-                    <div class="col-xs-12">
-                        <label class="label-control" for="nombre">Nombre</label>
-                        <input  class="form-control <?=($nombreError ? "has error" : "")?>" name="nombre" id="nombre" value="<?=$nombre?>"></input>
-                    </div>
-                </div>
-
-              <div class="form-group">
-                <div class="col-xs-12">
-                    <label class="label-control" for="description">Descripción</label>
-                    <textarea class="form-control" name="description" id="description" ><?=$description?></textarea>
-                    <button class="pull-right btn btn-lg sr-button">ENVIAR</button>
-                </div>
-              </div>
-          </form>
+            <?php endif; ?>
+           <?=$form->render();?>
         </div>
     </div>
- </div>
- 
- <?php
 
- include __DIR__ . "/partials/fin-doc.part.php";
+</div>
 
+<?php
+  include __DIR__ . "/partials/fin-doc.part.php";
 ?>
