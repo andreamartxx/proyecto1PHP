e440e4b089ed347d2fb1150cbffe6825191c3f8e
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-11-24 10:38:23 +0100

    v8_24nov

--- repository/UsuarioRepository.php
+++ repository/UsuarioRepository.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../database/QueryBuilder.php';
 class UsuarioRepository extends QueryBuilder
 {
     public function __construct(){
-        parent::__construct('usuarios', 'Usuario');
+        parent::__construct('users', 'Usuario');
     }
 
     /**
