ce1981e14888c20810e6bf857773be3b5c59734b
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-12-01 10:16:28 +0100

    v8_1dic

--- contact.php
+++ contact.php
@@ -13,46 +13,7 @@
     require_once "./database/Connection.php";
     require_once "./repository/MensajeRepository.php";
 
-    $info = "";
-    $firstName = new InputElement('text');
-    $firstName
-      ->setName('firstName')
-      ->setId('firstName')
-      ->setValidator(new NotEmptyValidator('El campo first name no puede estar vacío', true));
-
-
-    $lastName = new InputElement('text');
-    $lastName
-      ->setName('lastName')
-      ->setId('lastName');
-
-    $name = new MyFormGroup([new MyFormControl($firstName, "First Name"), new MyFormControl($lastName, "Last Name")]);
-
-    $email = new EmailElement();
-    $email
-      ->setName('email')
-      ->setId('email');
-    
-    $emailWrapper = new MyFormGroup([new MyFormControl($email, 'Correo', 'col-xs-12')]);
-
-
-    $subject = new InputElement('text');
-    $subject
-      ->setName('subject')
-      ->setId('subject')<?php
-    $title = "Contact";
-    require_once "./utils/utils.php";
-    require_once "./utils/Forms/InputElement.php";
-    require_once "./utils/Forms/EmailElement.php";
-    require_once "./utils/Forms/TextareaElement.php";
-    require_once "./utils/Forms/ButtonElement.php";
-    require_once "./utils/Forms/FormElement.php";
-    require_once "./utils/Forms/custom/MyFormGroup.php";
-    require_once "./utils/Forms/custom/MyFormControl.php";
-    require_once "./utils/Validator/NotEmptyValidator.php";
-    require_once "./entity/Mensaje.php";
-    require_once "./database/Connection.php";
-    require_once "./repository/MensajeRepository.php";
+    session_start();
 
     $info = "";
     $firstName = new InputElement('text');
@@ -104,72 +65,22 @@
      ->appendChild($messageWrapper)
      ->appendChild($b);
 
-    $config = require_once 'App/config.php';
+    $config = require_once 'app/config.php';
     App::bind('config', $config);
     App::bind('connection', Connection::make($config['database']));
-
-    $repositorio = new MensajeRepository();
-
-     if ("POST" === $_SERVER["REQUEST_METHOD"]) {
-        $form->validate();
-        if (!$form->hasError()) {
-          
-          $mensaje = new Mensaje($firstName->getValue(), $lastName->getValue(), $subject->getValue(), $email->getValue(), $message->getValue());
-          $repositorio->save($mensaje);
-          $info = "Mensaje insertado correctamente:";
-          $form->reset();
-        }else{
-          if ($firstName->hasError()) {
-            $firstName->setCssClass($firstName->getCssClass() . ' has-error');
-          }
-          if ($subject->hasError()) {
-            $subject->setCssClass($subject->getCssClass() . ' has-error');
-          }
-          if ($email->hasError()) {
-            $email->setCssClass($email->getCssClass() . ' has-error');
-          }
-        }
-    }
-  include("./views/contact.view.php");
-
-      ->setValidator(new NotEmptyValidator('El campo asunto no puede estar vacío', true));
-
-    $subjectWrapper = new MyFormGroup([new MyFormControl($subject, 'Asunto', 'col-xs-12')]);
-
-    $message = new TextareaElement();
-    $message
-     ->setName('message')
-     ->setId('message');
-    $messageWrapper = new MyFormGroup([new MyFormControl($message, 'Mensaje', 'col-xs-12')]);
-
-    $b = new ButtonElement('Send');
-    $b->setCssClass('pull-right btn btn-lg sr-button');
-
-    $form = new FormElement();
     
-    $form
-     ->setCssClass('form-horizontal')
-     ->appendChild($name)
-     ->appendChild($emailWrapper)
-     ->appendChild($subjectWrapper)
-     ->appendChild($messageWrapper)
-     ->appendChild($b);
-
-    $config = require_once 'App/config.php';
-    App::bind('config', $config);
-    App::bind('connection', Connection::make($config['database']));
     $repositorio = new MensajeRepository();
 
      if ("POST" === $_SERVER["REQUEST_METHOD"]) {
         $form->validate();
         if (!$form->hasError()) {
           
-          /*Grabamos en la base de datos*/
+          //grabamos en la base de datos
           $mensaje = new Mensaje($firstName->getValue(), $lastName->getValue(), $subject->getValue(), $email->getValue(), $message->getValue());
           $repositorio->save($mensaje);
           $info = "Mensaje insertado correctamente:";
           $form->reset();
-            
+          
         }else{
           if ($firstName->hasError()) {
             $firstName->setCssClass($firstName->getCssClass() . ' has-error');
